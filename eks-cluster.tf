6040d5701912290657fb5342eae7f6498df216fc
Anderson-Ferreira-Costa <andersonferreiracosta@outlook.com>
2022-08-17 13:08:18 -0300

    ok

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = local.cluster_name
-  cluster_version = "1.19"
+  cluster_version = "1.23"
   subnets         = module.vpc.private_subnets
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
